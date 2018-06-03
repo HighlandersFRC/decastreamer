abbbf416d1df265f13800cad9ae9da972cadcc4e
Jacob6868 <35152763+Jacob6868@users.noreply.github.com>
2018-06-03 14:38:53 -0600

    Add files via upload
    its kind of works. I usually have to start a putty window to the serial port, get coordinates pulling with "lep", and then close putty and start the app. You have to push the button in the upper right hand corner to start the data collection. You will probably have to change the serial port name/id

---
+++ UWBPlotter_Forms/UWBPlotter_Forms/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace UWBPlotter_Forms
+{
+    partial class Plot1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Plot1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.Anchor1 = new System.Windows.Forms.PictureBox();
+            this.Anchor3 = new System.Windows.Forms.PictureBox();
+            this.Anchor4 = new System.Windows.Forms.PictureBox();
+            this.Anchor2 = new System.Windows.Forms.PictureBox();
+            this.tag0 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.Anchor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Anchor3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Anchor4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Anchor2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tag0)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Reading";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Anchor1
+            // 
+            this.Anchor1.AccessibleName = "0,0";
+            this.Anchor1.Image = ((System.Drawing.Image)(resources.GetObject("Anchor1.Image")));
+            this.Anchor1.Location = new System.Drawing.Point(850, 439);
+            this.Anchor1.Name = "Anchor1";
+            this.Anchor1.Size = new System.Drawing.Size(50, 50);
+            this.Anchor1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Anchor1.TabIndex = 1;
+            this.Anchor1.TabStop = false;
+            // 
+            // Anchor3
+            // 
+            this.Anchor3.AccessibleName = "13.78, 5.56";
+            this.Anchor3.Image = ((System.Drawing.Image)(resources.GetObject("Anchor3.Image")));
+            this.Anchor3.Location = new System.Drawing.Point(150, 700);
+            this.Anchor3.Name = "Anchor3";
+            this.Anchor3.Size = new System.Drawing.Size(50, 50);
+            this.Anchor3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Anchor3.TabIndex = 2;
+            this.Anchor3.TabStop = false;
+            // 
+            // Anchor4
+            // 
+            this.Anchor4.AccessibleName = "-2.25, 5.58";
+            this.Anchor4.Image = ((System.Drawing.Image)(resources.GetObject("Anchor4.Image")));
+            this.Anchor4.Location = new System.Drawing.Point(507, 354);
+            this.Anchor4.Name = "Anchor4";
+            this.Anchor4.Size = new System.Drawing.Size(50, 50);
+            this.Anchor4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Anchor4.TabIndex = 3;
+            this.Anchor4.TabStop = false;
+            // 
+            // Anchor2
+            // 
+            this.Anchor2.AccessibleName = "13.78, 0.53";
+            this.Anchor2.Image = ((System.Drawing.Image)(resources.GetObject("Anchor2.Image")));
+            this.Anchor2.Location = new System.Drawing.Point(173, 312);
+            this.Anchor2.Name = "Anchor2";
+            this.Anchor2.Size = new System.Drawing.Size(50, 50);
+            this.Anchor2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Anchor2.TabIndex = 4;
+            this.Anchor2.TabStop = false;
+            // 
+            // tag0
+            // 
+            this.tag0.Image = ((System.Drawing.Image)(resources.GetObject("tag0.Image")));
+            this.tag0.Location = new System.Drawing.Point(377, 388);
+            this.tag0.Name = "tag0";
+            this.tag0.Size = new System.Drawing.Size(50, 50);
+            this.tag0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tag0.TabIndex = 5;
+            this.tag0.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 31);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Stop Reading";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(293, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(506, 227);
+            this.panel1.TabIndex = 7;
+            // 
+            // Plot1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 744);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tag0);
+            this.Controls.Add(this.Anchor2);
+            this.Controls.Add(this.Anchor4);
+            this.Controls.Add(this.Anchor3);
+            this.Controls.Add(this.Anchor1);
+            this.Controls.Add(this.button1);
+            this.Name = "Plot1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Plot_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.Anchor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Anchor3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Anchor4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Anchor2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tag0)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox Anchor1;
+        private System.Windows.Forms.PictureBox Anchor3;
+        private System.Windows.Forms.PictureBox Anchor4;
+        private System.Windows.Forms.PictureBox Anchor2;
+        private System.Windows.Forms.PictureBox tag0;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ UWBPlotter_Forms/UWBPlotter_Forms/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UWBPlotter_Forms
+{
+    public partial class Plot1 : Form
+    {
+        int screenHeight;
+        int screenWidth;
+        double scalingFactor;
+        double x0;
+        double y0;
+        SerialPort sp = new SerialPort();
+        bool _continue = true;
+        double modifiedAlpha;
+        double alpha = 0;
+        bool isFirstTime = true;
+        bool isFirstTimeRead = true;
+        double xCoord, yCoord, zCoord = 0;
+        double xtempPrev, ytempPrev = 0;
+        Pen pen = new Pen(Brushes.DeepSkyBlue, 5);
+        //Graphics formGraphics;
+        double[] tempPrevCoords = new double[2];
+        double[] tempCurrentCoords = new double[2];
+        public Plot1()
+        {
+            InitializeComponent();
+            screenHeight = 800;
+            screenWidth = 1000;
+            scalingFactor = screenWidth / 15;
+            x0 = 3 * scalingFactor;
+            y0 = 2 * scalingFactor;
+            List<PictureBox> Anchors = new List<PictureBox>();
+            Anchors.Add(Anchor1);
+            Anchors.Add(Anchor2);
+            Anchors.Add(Anchor3);
+            Anchors.Add(Anchor4);
+            foreach (PictureBox Anchor in Anchors)
+            {
+                string name = Anchor.AccessibleName;
+                string[] coords = name.Split(',');
+                double[] coord = new double[2];
+                coord[0] = double.Parse(coords[0]);
+                coord[1] = double.Parse(coords[1]);
+                int[] scaledPoint = scalePoint(coord);
+                Anchor.Location = new Point(scaledPoint[0], scaledPoint[1]);
+                Anchor.BackColor = Color.Transparent;
+            }
+        }
+
+        public int[] scalePoint(double[] coord)
+        {
+            int[] scaledCoord = new int[2];
+            scaledCoord[0] = Int32.Parse(Math.Round(x0 + coord[0] * scalingFactor).ToString());
+            scaledCoord[1] = Int32.Parse(Math.Round(800-(y0 + coord[1] * scalingFactor)).ToString());
+            return scaledCoord;
+        }
+
+        private void Plot_Paint(object sender, PaintEventArgs e)
+        {
+            //System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+            //System.Drawing.Graphics formGraphics;
+            //formGraphics = this.CreateGraphics();
+            //formGraphics.DrawLine(myPen, 0, 0, 200, 200);
+            //myPen.Dispose();
+            //formGraphics.Dispose();
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            bool isFirstTime = true;
+            bool isSecondTime = false;
+            string line;
+
+            // set some port parameters
+            sp.BaudRate = 115200;
+            //sp.DataBits = 8;
+            //sp.Parity = Parity.None;
+            //sp.StopBits = StopBits.One;
+            sp.ReadTimeout = 2000;
+            sp.PortName = "COM8";
+            try
+            {
+                sp.Open();
+            }
+            catch
+            {
+                //MessageBox.Show("Error opening serial port");
+                return;
+            }
+            int i = 0;
+            while (true)
+            {
+                try
+                {
+                    line = sp.ReadLine();  // Will read until carriage-return
+                    ProcessLine(line); // Your method to parse the line
+                    isFirstTime = false;
+                    sp.DiscardInBuffer();
+                }
+                catch
+                {
+                    sp.Write("\r");
+                    if (isSecondTime)
+                    {
+                        isFirstTime = false;
+                        continue;
+                    }
+                    if (isFirstTime)
+                    {
+                        //sp.Close();
+                        //sp.Open();
+                        System.Threading.Thread.Sleep(500);
+                        sp.Write("nmt\r\r");
+                        System.Threading.Thread.Sleep(2000);
+                        sp.Write("lep\r");
+                    }
+                    System.Threading.Thread.Sleep(20);
+                    isSecondTime = false;
+                    isFirstTime = false;
+                }
+            }
+            sp.Close();
+        }
+
+        public void ProcessLine(string line)
+        {
+            //POS[10, 2, 3, 100]
+            //POS,6.63,3.74,-1.86,100
+
+            if (line != "")
+            {
+                string[] coords = line.Split(',');
+                double xCoordCurrent = double.Parse(coords[1]);
+                double yCoordCurrent = double.Parse(coords[2]);
+                double zCoordCurrent = double.Parse(coords[3]);
+                int confid = int.Parse(coords[4]);
+                modifiedAlpha = (1 - alpha) * confid / 100;
+                if (isFirstTimeRead)
+                    modifiedAlpha = 1;
+                modifiedAlpha = 1;
+                xCoord = xCoordCurrent * (modifiedAlpha) + xCoord * (1 - modifiedAlpha);
+                yCoord = yCoordCurrent * (modifiedAlpha) + yCoord * (1 - modifiedAlpha);
+                //formGraphics = this.CreateGraphics();
+                tempPrevCoords[0] = xtempPrev;
+                tempPrevCoords[1] = ytempPrev;
+                int[] scaledPrevPoint = scalePoint(tempPrevCoords);
+                tempCurrentCoords[0] = xCoord;
+                tempCurrentCoords[1] = yCoord;
+                int[] scaledCurrentPoint = scalePoint(tempCurrentCoords);
+
+                System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+                System.Drawing.Graphics formGraphics;
+                formGraphics = this.CreateGraphics();
+                formGraphics.DrawLine(myPen, scaledPrevPoint[0], scaledPrevPoint[1], scaledCurrentPoint[0], scaledCurrentPoint[1]);
+                myPen.Dispose();
+                formGraphics.Dispose();
+
+                //if (!isFirstTimeRead)
+                //formGraphics.DrawLine(pen, scaledPrevPoint[0], scaledPrevPoint[1], scaledCurrentPoint[0], scaledCurrentPoint[1]);
+                xtempPrev = xCoord;
+                ytempPrev = yCoord;
+                //tag0.Location = new Point(scaledCurrentPoint[0], scaledCurrentPoint[1]);
+                //zCoord = zCoordCurrent * (modifiedAlpha) + zCoord * (1 - modifiedAlpha);
+                isFirstTimeRead = false;
+                //double[] coord = new double[2];
+                //coord[0] = xCoord;
+                //coord[1] = yCoord;
+                //int[] scaledPoint = scalePoint(coord);
+                //tag0.Location = new Point(scaledPoint[0], scaledPoint[1]);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sp.Close();
+            _continue = false;
+        }
+    }
+}
